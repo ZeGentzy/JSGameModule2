79d2c5c5a20802064dc7b1a49bdd10b5b45f7e40
Hal Gentz <zegentzy@protonmail.com>
2018-10-10 06:41:13 -0600

    Base
    Signed-off-by: Hal Gentz <zegentzy@protonmail.com>

--- runProgram.sh
+++ runProgram.sh
@@ -3,4 +3,4 @@ APPDIR=$(dirname "$0")
 cd "$APPDIR"
 #java -jar js.jar -f "myprogram.js" -opt -1
 java -cp js.jar:lanterna-3.0.1.jar org.mozilla.javascript.tools.shell.Main -f "myprogram.js" -opt 9
-$SHELL
+#$SHELL
